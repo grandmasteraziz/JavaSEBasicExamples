6236fb3ee5d20228b514cae324022522b42a3e15
grandmasteraziz <grandmasteraziz@hotmail.com>
2017-06-18 18:12:58 +0300

    mymessage

---
+++ src/com/wordpress/azizfarmer/languageBasic/CircumferenceOfCircle.java
@@ -0,0 +1,20 @@
+package com.wordpress.azizfarmer.languageBasic;
+
+public class CircumferenceOfCircle {
+	
+	 
+  
+	public double calculate(double radius)
+	{
+	  double PI = 3.14;
+		
+	  return PI*radius*2;
+	}
+	
+	
+    
+	
+	
+
+
+}
---
+++ src/com/wordpress/azizfarmer/languageBasic/Test.java
@@ -0,0 +1,14 @@
+package com.wordpress.azizfarmer.languageBasic;
+
+public class Test {
+	
+	public static void main(String[] args) {
+
+		CircumferenceOfCircle cOC = new CircumferenceOfCircle();
+		
+		
+		System.out.println(cOC.calculate(5));
+
+	}
+
+}
